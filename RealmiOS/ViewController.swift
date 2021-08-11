d709b792acd38f239c9b18a8fbb470b90caa6100
ogoogo <ogosae.1018173@icloud.com>
2021-08-11 13:07:17 +0900

    Initial Commit

---
+++ RealmiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmiOS
+//
+//  Created by 生越冴恵 on 2021/08/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
